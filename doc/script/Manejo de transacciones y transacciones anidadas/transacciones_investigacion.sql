111d6dceddbd10511576a2352b41e96e89ff4b98
jcsa87 <juancruzsenicenacosta@gmail.com>
2025-11-05 17:30:53 -0300

    creación de script.sql

---
+++ doc/script/Manejo de transacciones y transacciones anidadas/transacciones_investigacion.sql
@@ -0,0 +1,13 @@
+/*
+================================================================
+- Base de Datos I
+
+- Investigación sobre Manejo de Transacciones
+y Transacciones Anidadas
+
+- Grupo 34
+================================================================
+*/
+
+USE servicio_tecnico_informatico;
+GO
